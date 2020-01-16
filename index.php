41e7c1f12b57ef1fc189ec0211cf534853eb56c6
Seryph <seryph@Seryphs-MacBook-Pro.local>
2020-01-16 14:31:06 -0600

    new

---
+++ index.php
@@ -0,0 +1,29 @@
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<div class="mainSection">
+
+    <div class="logoContainer">
+        <img src="./assets/img/doodleLogo.png" alt="" >
+    </div>
+
+    <div class="searchContainer">
+        <form action="search.php" method="GET">
+            <input type="text" class="searchBox" name="term">
+            <input type="submit">
+        </form>
+    </div>
+</div>
+    
+</body>
+</html>
