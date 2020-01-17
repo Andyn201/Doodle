dc24fda637223c4de604dad33f20d841635f9e78
Seryph <seryph@Seryphs-MacBook-Pro.local>
2020-01-17 14:49:21 -0600

    first commit

--- crawl.php
+++ crawl.php
@@ -4,6 +4,18 @@ include("classes/DomDocumentParser.php");
 
 $alreadyCrawled = array();
 $crawling = array();
+$alreadyFoundImages = array();
+
+function linkExists($url) {
+	global $con;
+
+	$query = $con->prepare("SELECT * FROM sites WHERE url = :url");
+
+	$query->bindParam(":url", $url);
+	$query->execute();
+
+	return $query->rowCount() != 0;
+}
 
 function insertLink($url, $title, $description, $keywords) {
 	global $con;
@@ -11,23 +23,24 @@ function insertLink($url, $title, $description, $keywords) {
 	$query = $con->prepare("INSERT INTO sites(url, title, description, keywords)
 							VALUES(:url, :title, :description, :keywords)");
 
-    if(!$url){
-        $url = "";
-    }
-    else if (!$title){
-        $title = "";
-    }
-    else if (!$description){
-        $description = "";
-    }
-    else if (!$keywords){
-        $keywords = "";
-    }
-
 	$query->bindParam(":url", $url);
 	$query->bindParam(":title", $title);
 	$query->bindParam(":description", $description);
 	$query->bindParam(":keywords", $keywords);
+
+	return $query->execute();
+}
+
+function insertImage($url, $src, $alt, $title) {
+	global $con;
+
+	$query = $con->prepare("INSERT INTO images(siteUrl, imageUrl, alt, title)
+							VALUES(:siteUrl, :imageUrl, :alt, :title)");
+
+	$query->bindParam(":siteUrl", $url);
+	$query->bindParam(":imageUrl", $src);
+	$query->bindParam(":alt", $alt);
+	$query->bindParam(":title", $title);
 
 	return $query->execute();
 }
@@ -57,6 +70,8 @@ function createLink($src, $url) {
 }
 
 function getDetails($url) {
+
+	global $alreadyFoundImages;
 
 	$parser = new DomDocumentParser($url);
 
@@ -93,7 +108,36 @@ function getDetails($url) {
 	$keywords = str_replace("\n", "", $keywords);
 
 
-	insertLink($url, $title, $description, $keywords);
+	if(linkExists($url)) {
+		echo "$url already exists<br>";
+	}
+	else if(insertLink($url, $title, $description, $keywords)) {
+		echo "SUCCESS: $url<br>";
+	}
+	else {
+		echo "ERROR: Failed to insert $url<br>";
+	}
+
+	$imageArray = $parser->getImages();
+	foreach($imageArray as $image) {
+		$src = $image->getAttribute("src");
+		$alt = $image->getAttribute("alt");
+		$title = $image->getAttribute("title");
+
+		if(!$title && !$alt) {
+			continue;
+		}
+
+		$src = createLink($src, $url);
+
+		if(!in_array($src, $alreadyFoundImages)) {
+			$alreadyFoundImages[] = $src;
+
+			echo "INSERT: " . insertImage($url, $src, $alt, $title);
+		}
+
+	}
+
 
 }
 
@@ -126,8 +170,6 @@ function followLinks($url) {
 
 			getDetails($href);
 		}
-		else return;
-
 
 	}
 
@@ -139,6 +181,6 @@ function followLinks($url) {
 
 }
 
-$startUrl = "http://www.bbc.com";
+$startUrl = "https://www.bbc.com/";
 followLinks($startUrl);
 ?>
