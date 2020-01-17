d51f4461f6ac002f2d2d1b6ea00f205989c8a764
Seryph <seryph@Seryphs-MacBook-Pro.local>
2020-01-17 16:00:06 -0600

    first commit

--- search.php
+++ search.php
@@ -1,70 +1,106 @@
 <?php
+include("config.php");
+include("classes/SiteResultsProvider.php");
 
-include('config.php');
+if(isset($_GET["term"])) {
+	$term = $_GET["term"];
+}
+else {
+	exit("You must enter a search term");
+}
 
-    if(isset($_GET["term"])){
-        $term = $_GET['term'];
-    }
+$type = isset($_GET["type"]) ? $_GET["type"] : "sites";
 
-    else {
 
-    }
-
-    $type = isset($_GET["type"]) ? $term = $_GET["type"] : "sites";
-
+	
 ?>
-
-
-
-
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <title>Noodle</title>
+	<title>Welcome to Doodle</title>
+
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+
 </head>
 <body>
 
-<div class="wrapper ">
+	<div class="wrapper">
+	
+		<div class="header">
 
-    <div class="header">
 
-        <div class="headerContent">
+			<div class="headerContent">
 
-            <div class="logoContainer">
-                <a href="index.php"><img src="./assets/img/logo.png" alt="" ></a>
-            </div>
+				<div class="logoContainer">
+					<a href="index.php">
+						<img src="./assets/img/logo.png">
+					</a>
+				</div>
 
-            <div class="searchContainer">
-                <form action="search.php" method="GET">
-                    <div class="searchBarContainer">
-                        <input type="text" class="searchBox" name="term">
-                        <button class="searchButton">
-                            <img src="./assets/img/icons/search.png" alt="logo">
-                        </button>
-                    </div>
-                </form>
-            </div>
+				<div class="searchContainer">
 
-        </div>
+					<form action="search.php" method="GET">
 
-        <div class="tabsContainer">
-            <ul class="tabList">
-                <li class='<?php echo $type == "sites" ? "active" : "" ?>''>
-                    <a href='<?php echo "search.php?term=$term&type=sites"; ?>'>Sites</a>
-                </li>
-                <li class='<?php echo $type == "images" ? "active" : "" ?>''>
-                    <a href='<?php echo "search.php?term=$term&type=images"; ?>'>Images</a>
-                </li>
-            </ul>
-        </div>
-        
-    </div>
+						<div class="searchBarContainer">
 
-</div>
-    
+							<input class="searchBox" type="text" name="term">
+							<button class="searchButton">
+								<img src="assets/images/icons/search.png">
+							</button>
+						</div>
+
+					</form>
+
+				</div>
+
+			</div>
+
+
+			<div class="tabsContainer">
+
+				<ul class="tabList">
+
+					<li class="<?php echo $type == 'sites' ? 'active' : '' ?>">
+						<a href='<?php echo "search.php?term=$term&type=sites"; ?>'>
+							Sites
+						</a>
+					</li>
+
+					<li class="<?php echo $type == 'images' ? 'active' : '' ?>">
+						<a href='<?php echo "search.php?term=$term&type=images"; ?>'>
+							Images
+						</a>
+					</li>
+
+				</ul>
+
+
+			</div>
+		</div>
+
+
+
+
+
+
+
+
+
+
+		<div class="mainResultsSection">
+
+			<?php
+			$resultsProvider = new SiteResultsProvider($con);
+
+			echo $resultsProvider->getNumResults($term);
+			?>
+
+
+		</div>
+
+
+
+	</div>
+
 </body>
 </html>
