4a592b7426c38a6f75b7d76cf6b1e3804d96f585
Seryph <seryph@Seryphs-MacBook-Pro.local>
2020-01-16 21:51:26 -0600

    new

---
+++ crawl.php
@@ -0,0 +1,15 @@
+<?php
+
+include("./classes/DomDocumentParser.php");
+
+
+function followLinks($url) {
+
+    $parser = new DomDocumentParser($url);
+
+}
+
+
+$startUrl = "http://www.reecekenney.com";
+
+followLinks($startUrl);
